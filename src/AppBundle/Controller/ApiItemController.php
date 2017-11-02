a183f8cf994d2b4c5db3479b1865f822350bdbf3
pavol.ipoth@gmail.com <pavol.ipoth@gmail.com>
2017-11-02 21:48:26 +0100

    Create controller for item resource and get action

---
+++ src/AppBundle/Controller/ApiItemController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\FOSRestController;
+use Elastica\Query\Range;
+use Elastica\Query;
+use Elastica\Query\QueryString;
+use Elastica\Filter;
+use Elastica\QueryBuilder;
+use DateTime;
+use Elastica\Request as ElasticaRequest;
+
+class ApiItemController extends FOSRestController
+{
+    public function itemsAction(Request $request)
+    {
+        $results = [];
+        $hostQuery = new Query();
+                
+        
+        $filterByTerm = $request->query->get('filterByTerm');
+        $sortField = $request->query->get('sortField');
+        $sort = $request->query->get('sort');
+        $page = $request->query->get('page');
+        $size = $request->query->get('size');
+        $from = ($page-1) * $size;  
+        
+        $size = 10000;
+        $from = 0;
+        
+        $sort = $sort ? $sort : 'asc';
+        $sortField = $sortField ? $sortField : 'ansible_hostname';
+        
+        $client = $this->get('fos_elastica.client.default');
+        
+        $elasticIndashIndex = $client->getIndex('indash');
+        $elasticType = $elasticIndashIndex->getType('host');
+
+        $query = 
+        [
+            "size" => 0,
+            "from" => $from,
+            "filter" => 
+            [
+                "query" => 
+                [
+                    "match_all" => []
+                ]
+            ], 
+            "aggs" => 
+            [
+                "product" => 
+                [
+                    "terms" => 
+                    [
+                        "field" => "$sortField",
+                        "size" => $size
+                    ],
+                    "aggs" => 
+                    [
+                        "machine" => 
+                        [
+                            "terms" => 
+                            [
+                                "field" => "ansible_product_uuid"
+                            ],
+                            "aggs" => 
+                            [
+                                "hosts" => 
+                                [
+                                    "top_hits" => 
+                                    [
+                                        "_source" => 
+                                        [
+                                            "include" => 
+                                            [
+                                                "*"
+                                            ]
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]  
+                ]      
+            ]
+        ];
+        
+        $path = $elasticIndashIndex->getName() . '/' . $elasticType->getName() . '/_search';
+
+        $response = $client->request($path, ElasticaRequest::GET, $query);
+        $results = $response->getData();
+
+        foreach($results['aggregations']['product']['buckets'] as $item)
+        {
+            $result = $item['machine']['buckets'][0]['hosts']['hits']['hits'][0];
+            $resultArray = [];
+
+            $resultArray['type'] = 'item';
+            $resultArray['_id'] = $result['_source']['ansible_product_uuid'];
+            $resultArray['attributes'] = $result['_source'];
+
+            $resultsArray['data'][] = $resultArray;
+        }
+
+        $view = $this->view($resultsArray, 200)->setFormat("jsonapi");
+
+        return $this->handleView($view);
+    }
+}
